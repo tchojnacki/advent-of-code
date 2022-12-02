9889c41f556eb134527909b7e0f29224786d33ec
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2022-12-02 18:43:39 +0100

    Upload first two days of year 2022

---
+++ aoc-2022-dotnet/Day01/Program.fs
@@ -0,0 +1,37 @@
+﻿open System.IO
+
+let parseLine = function "" -> None | l -> Some(int l)
+let readInput path = File.ReadLines path |> Seq.map parseLine
+
+let splitOnNone xs =
+    let (lastGroup, result) = Seq.fold (fun (groupAcc, resultAcc) x ->
+        match x with
+        | Some(value) -> (value :: groupAcc, resultAcc)
+        | None -> (List.empty, (List.rev groupAcc) :: resultAcc)) (List.empty, List.empty) xs
+    in List.rev (if List.isEmpty lastGroup then result else lastGroup :: result)
+
+let groupCalorySums data = data |> splitOnNone |> List.map List.sum
+
+let mostTotalCalories data = data |> groupCalorySums |> List.max
+
+let rec insert x xs =
+  match xs with
+    | [] -> [x]
+    | y::ys -> if x < y then x::y::ys else y::insert x ys
+
+let topN n data =
+    Seq.fold (fun acc elem ->
+        if List.length acc < n then insert elem acc
+        elif List.head acc < elem then insert elem (List.tail acc)
+        else acc
+    ) List.empty data
+
+let top3TotalCaloriesSum data = data |> groupCalorySums |> topN 3 |> List.sum
+
+let test = readInput "test.txt"
+assert (mostTotalCalories test = 24000)
+assert (top3TotalCaloriesSum test = 45000)
+
+let input = readInput "input.txt"
+printfn "%d" (mostTotalCalories input)
+printfn "%d" (top3TotalCaloriesSum input)
---
+++ aoc-2022-dotnet/Day02/Program.fs
@@ -0,0 +1,53 @@
+﻿open System.IO
+open FSharpPlus
+
+type Move = R | P | S
+type Result = Win | Draw | Lose
+exception ParseError
+
+let parseEnemyMove = function "A" -> R | "B" -> P | "C" -> S | _ -> raise ParseError
+let parseAllyMove = function "X" -> R | "Y" -> P | "Z" -> S | _ -> raise ParseError
+let parseOutcome = function "X" -> Lose | "Y" -> Draw | "Z" -> Win | _ -> raise ParseError
+
+let roundScore round =
+    let selectionScore = function R -> 1 | P -> 2 | S -> 3
+
+    let outcomeScore = function
+        | (R, S) | (S, P) | (P, R) -> 0
+        | (R, R) | (P, P) | (S, S) -> 3
+        | (R, P) | (P, S) | (S, R) -> 6
+
+    selectionScore (snd round) + outcomeScore round
+
+let lineToTuple line =
+    match String.split [" "] line |> Seq.toList with
+    | [first; second] -> first, second
+    | _ -> raise ParseError
+
+let selectMove outcome enemyMove =
+    match (outcome, enemyMove) with
+    | (Win, R) -> P | (Draw, R) -> R | (Lose, R) -> S
+    | (Win, P) -> S | (Draw, P) -> P | (Lose, P) -> R
+    | (Win, S) -> R | (Draw, S) -> S | (Lose, S) -> P
+
+let parseRoundV1 roundStr =
+    let (firstStr, secondStr) = lineToTuple roundStr
+    parseEnemyMove firstStr, parseAllyMove secondStr
+
+let parseRoundV2 roundStr =
+    let (firstStr, secondStr) = lineToTuple roundStr
+    let enemyMove = parseEnemyMove firstStr
+    let outcome = parseOutcome secondStr
+    let allyMove = selectMove outcome enemyMove
+    enemyMove, allyMove
+
+let rateStrategyGuideV1 input = input |> Seq.map parseRoundV1 |> Seq.sumBy roundScore
+let rateStrategyGuideV2 input = input |> Seq.map parseRoundV2 |> Seq.sumBy roundScore
+
+let test = File.ReadLines "test.txt"
+assert (rateStrategyGuideV1 test = 15)
+assert (rateStrategyGuideV2 test = 12)
+
+let input = File.ReadLines "input.txt"
+printfn "%d" (rateStrategyGuideV1 input)
+printfn "%d" (rateStrategyGuideV2 input)
