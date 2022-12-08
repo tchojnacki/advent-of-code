65844a7ff88870f1336ccc10ceaba3b4c3bc2a25
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2022-12-08 19:45:35 +0100

    Setup project template and generate day 9 files

---
+++ aoc-2022-dotnet/AoC.Day.Template/Program.fs
@@ -0,0 +1,7 @@
+﻿module AoC.Day.Template
+
+open System.IO
+
+let test = File.ReadLines("test.txt")
+
+let input = File.ReadLines("input.txt")
---
+++ aoc-2022-dotnet/Day09/Program.fs
@@ -0,0 +1,7 @@
+﻿module Day09
+
+open System.IO
+
+let test = File.ReadLines("test.txt")
+
+let input = File.ReadLines("input.txt")
